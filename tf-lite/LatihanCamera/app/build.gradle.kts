4c27ca90480307acba9c3125001fad0bc996573c
Chairoel <projectchairul@gmail.com>
2026-01-19 19:16:51 +0700

    update tensorflow dependencies

--- tf-lite/LatihanCamera/app/build.gradle.kts
+++ tf-lite/LatihanCamera/app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation("androidx.camera:camera-view:$cameraxVersion")
 
     //default
-    implementation("org.tensorflow:tensorflow-lite-support:0.1.0")
-    implementation("org.tensorflow:tensorflow-lite-metadata:0.1.0")
+    implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
 }
